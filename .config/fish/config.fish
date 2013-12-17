33ee8b8813ec4998800e49f109dac47ccba9559b
Tilman Potthof <tilmanpotthof@googlemail.com>
2013-12-17 11:49:26 +0100

    oh-my-fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,24 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+set fish_theme robbyrussell
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
+
+# Path to your custom folder (default path is $FISH/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+# aliases
 alias 'll' 'ls -la'
 
-set MAVEN_OPTS "-Xms256m -Xmx2048m"
+# homebrew binaries first
 set PATH "/usr/local/bin" $PATH
+
+# more java memory
+set MAVEN_OPTS "-Xms256m -Xmx2048m"
